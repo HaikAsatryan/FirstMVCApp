3dfa00c2e95e39130351342a29528dbaabed28f5
HaikAsatryan <Haik.asatryan.95@gmail.com>
2022-12-04 23:27:40 +0400

    Add project files.

---
+++ FirstMVCApp/Controllers/DogSoftController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FirstMVCApp.Models;
+
+namespace FirstMVCApp.Controllers;
+
+public class DogSoftController : Controller
+{
+    private static List<DogViewModel> dogs = new List<DogViewModel>();
+
+    public IActionResult Index()
+    {
+        //var doggo = new DogViewModel() { Name = "Sif", Age = 2 };
+        return View(dogs);
+    }
+
+    public IActionResult Create()
+    {
+        var dogVm = new DogViewModel();
+        return View(dogVm);
+    }
+
+    public IActionResult CreateDog(DogViewModel dogViewModel)
+    {
+        dogs.Add(dogViewModel);
+        return RedirectToAction(nameof(Index));
+    }
+
+    public string Hello()
+    {
+        return "Who's there?";
+    }
+}
